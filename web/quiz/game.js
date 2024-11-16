f19be3eef79bb4486e92a051e1f4074ef617110b
Héctor <hector.fernandez.de.sevilla@estudiantat.upc.edu>
2024-11-16 13:43:45 +0100

    a la mierda react

---
+++ web/quiz/game.js
@@ -0,0 +1,142 @@
+const question = document.querySelector ('#question');
+const choices = Array.from(document.querySelectorAll ('.choice-text'));
+const img = document.querySelector ('#question-pic');
+const progressText = document.querySelector ('#progressText');
+const scoreText = document.querySelector ('#score');
+const progressBarFull = document.querySelector ('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: "Respon a la pregunta 1",
+        choice1: "Real",
+        choice2: "Phishing",
+        answer: 1,
+    },
+    {
+        question: "Respón a la pregunta 2",
+        choice1: "Real",
+        choice2: "Phishing",
+        answer: 1,
+    },
+    {
+        question: "Respón a la pregunta 3",
+        choice1: "Real",
+        choice2: "Phishing",
+        answer: 1,
+    },
+    {
+        question: "Respón a la pregunta 4",
+        choice1: "Real",
+        choice2: "Phishing",
+        answer: 1,
+    },
+    {
+        question: "Respón a la pregunta 5",
+        choice1: "Real",
+        choice2: "Phishing",
+        answer: 1,
+    },
+    {
+        question: "Respón a la pregunta 6",
+        choice1: "Real",
+        choice2: "Phishing",
+        answer: 1,
+    },
+    {
+        question: "Respón a la pregunta 7",
+        choice1: "Real",
+        choice2: "Phishing",
+        answer: 1,
+    },
+    {
+        question: "Respón a la pregunta 8",
+        choice1: "Real",
+        choice2: "Phishing",
+        answer: 1,
+    },
+    {
+        question: "Respón a la pregunta 9",
+        choice1: "Real",
+        choice2: "Phishing",
+        answer: 1,
+    },
+    {
+        question: "Respón a la pregunta 10",
+        choice1: "Real",
+        choice2: "Phishing",
+        answer: 1,
+    },
+]
+
+const SCORE_POINTS = 1
+const MAX_QUESTIONS = 10
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('end.html')
+    }
+
+
+    questionCounter++
+    progressText.innerText = `Pregunta ${questionCounter} de ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame()
